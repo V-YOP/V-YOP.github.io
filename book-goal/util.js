5eb82b1af6b950a26f8338b80b48e816c125b7ca
V-YOP <V-YOP@pm.me>
2022-04-22 00:57:18 +0800

    Site updated: 2022-04-22 00:57:17

--- book-goal/util.js
+++ book-goal/util.js
@@ -5,13 +5,13 @@
  */
 Date.prototype.format = function (fmt) {
   const o = {
-    "M+": this.getMonth() + 1,                 //月份 
-    "d+": this.getDate(),                    //日 
-    "h+": this.getHours(),                   //小时 
-    "m+": this.getMinutes(),                 //分 
-    "s+": this.getSeconds(),                 //秒 
-    "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-    "S": this.getMilliseconds()             //毫秒 
+    "M+": this.getMonth() + 1,                     //月份 
+    "d+": this.getDate(),                          //日 
+    "h+": this.getHours(),                         //小时 
+    "m+": this.getMinutes(),                       //分 
+    "s+": this.getSeconds(),                       //秒 
+    "q+": Math.floor((this.getMonth() + 3) / 3),   //季度 
+    "S": this.getMilliseconds()                    //毫秒 
   };
 
   if (/(y+)/.test(fmt)) {
